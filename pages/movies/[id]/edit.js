66bcec19a20268406b039e779030d660b0bb8f89
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-12-10 14:35:21 +0100

    fix reloding by next/link router/link

--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -13,7 +13,7 @@ class EditMovie extends React.Component {
   handleUpdateMovie = movie => {
     updateMovie(movie).then(updateMovie => {
       //router.push('/');
-      Router.push(`/movies/${movie.id}`);
+      Router.push('/movies/[id]', `/movies/${movie.id}`);
     });
   };
   render() {
